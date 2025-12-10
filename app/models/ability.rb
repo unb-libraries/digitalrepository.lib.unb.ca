a45c3e7c2a45407ec01b8150bf11ccc23e96b6e7
Patrick Schilf <pschilf@unb.ca>
2025-12-10 12:08:42 -0400

    Define admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,16 +1,17 @@
 class Ability
   include Hydra::Ability
-  
+
   include Hyrax::Ability
-  self.ability_logic += [:everyone_can_create_curation_concerns]
+  self.ability_logic += [ :everyone_can_create_curation_concerns ]
 
   # Define any customized permissions here.
   def custom_permissions
     # Limits deleting objects to a the admin user
     #
-    # if current_user.admin?
-    #   can [:destroy], ActiveFedora::Base
-    # end
+    if current_user.admin?
+      # can [:destroy], ActiveFedora::Base
+      can [ :show, :add_user, :remove_user, :index ], Role
+    end
 
     # Limits creating new objects to a specific group
     #
