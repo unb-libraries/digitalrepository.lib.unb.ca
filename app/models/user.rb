b94712f7eb9fee9cc499606255a249e17ad7cf37
Patrick Schilf <pschilf@unb.ca>
2026-02-26 10:52:44 -0400

    Allow omniauth login only

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
 
   # Connects this user object to Blacklights Bookmarks.
   include Blacklight::User
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[saml]
+  # Include default devise modules.
+  # https://github.com/heartcombo/devise
+  devise :rememberable, :omniauthable, omniauth_providers: %i[saml]
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
@@ -26,7 +26,6 @@ class User < ApplicationRecord
 
     user = find_or_initialize_by(provider: auth.provider, uid: provider_info["eduPersonPrincipalName"][0])
     user.email = auth.info.email
-    user.password = Devise.friendly_token[0, 20]
     user.first_name = provider_info["givenName"][0]
     user.last_name = provider_info["sn"][0]
     user.display_name = "#{user.first_name} #{user.last_name}"
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
     concerns :searchable
   end
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_scope :user do
+    get "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
+  end
+
   mount Hydra::RoleManagement::Engine => '/'
 
   require "sidekiq/web"
