34b80f25c5d0808ab8587f3c3a2ed6320e80fd1a
Patrick Schilf <pschilf@unb.ca>
2026-03-10 13:16:44 -0300

    Fix production upload,cache path config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,8 +104,8 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   # Custom configuration for Hyrax
-  config.x.hyrax.upload_path = ->() { Rails.root + "storage" + "tmp" + "uploads" }
-  config.x.hyrax.cache_path = ->() { Rails.root + "storage" + "tmp" + "cache" }
+  config.x.hyrax.upload_path = Rails.root.join("storage", "tmp", "uploads")
+  config.x.hyrax.cache_path = Rails.root.join("storage", "tmp", "cache")
   config.x.hyrax.derivatives_path = Rails.root.join("storage", "derivatives")
   config.x.hyrax.working_path = Rails.root.join("storage", "tmp", "uploads")
 end
