8eb73bef863b688dd8a2a4e08c634cb3572d17d9
Patrick Schilf <pschilf@unb.ca>
2026-03-16 11:34:57 -0300

    Revert single-container job processing in local development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   config.active_job.verbose_enqueue_logs = true
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_adapter = :sidekiq
+  config.active_job.queue_adapter = :async
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
