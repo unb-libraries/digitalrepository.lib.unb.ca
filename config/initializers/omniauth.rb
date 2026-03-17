176153d11f2e8cd4bb77d60ac45f02219c2b77f1
Patrick Schilf <pschilf@unb.ca>
2026-03-17 16:26:46 -0300

    Set SAML SP service name

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :saml,
+    attribute_service_name: ENV.fetch("SAML_SP_SERVICE_NAME", "Digital Repository"),
     assertion_consumer_service_url: ENV["SAML_SP_ASSERTION_CONSUMER_SERVICE_URL"],
     sp_entity_id: ENV["SAML_SP_ENTITY_ID"],
     idp_sso_service_url: ENV["SAML_IDP_SSO_SERVICE_URL"],
