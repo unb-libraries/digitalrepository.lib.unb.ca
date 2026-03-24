8a5720bac36c7229215ca94d0e5238bd3ca69db1
Patrick Schilf <pschilf@unb.ca>
2026-03-24 13:42:47 -0300

    User model schema require uid, unrequire email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_25_173056) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_23_145531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
@@ -546,7 +546,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_25_173056) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: ""
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -578,11 +578,10 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_25_173056) do
     t.binary "zotero_token"
     t.string "zotero_userid"
     t.string "preferred_locale"
-    t.string "provider"
-    t.string "uid"
+    t.string "provider", default: "saml", null: false
+    t.string "uid", null: false
     t.string "first_name"
     t.string "last_name"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
