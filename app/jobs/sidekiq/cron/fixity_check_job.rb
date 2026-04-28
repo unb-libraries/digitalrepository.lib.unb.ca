95f037bf3286baee3dfa1aed6db93a46dc2c5db1
Patrick Schilf <pschilf@unb.ca>
2026-04-28 14:44:20 -0300

    Schedule fixity check job

---
+++ app/jobs/sidekiq/cron/fixity_check_job.rb
@@ -0,0 +1,16 @@
+module Sidekiq
+  module Cron
+    class FixityCheckJob
+      include Sidekiq::Worker
+
+      def perform
+        Hyrax.logger.info "Starting fixity check job"
+        Hyrax.query_service.find_all_of_model(model: Hyrax::FileSet).each_slice(1000) do |slice|
+          slice.each do |fs|
+            Hyrax::FileSetFixityCheckService.new(fs, max_days_between_fixity_checks: -1).fixity_check
+          end
+        end
+      end
+    end
+  end
+end
